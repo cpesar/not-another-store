f207bfef48b7685b1568a5883cc40cc97f7be799
cpesar <cpesar1@gmail.com>
2025-01-17 00:45:59 -0700

    added light and dark modes with dropdown

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
 import { ShoppingBagIcon } from "@/images/shopping-bags";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -17,12 +18,13 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart /> Cart
             </Link>
           </Button>
-          <Button asChild variant="ghost">
+          <Button asChild>
             <Link href="/sign-in">
               <UserIcon /> Sign In
             </Link>
