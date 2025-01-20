4f693aa289e237dee1c7218178473cc49434d4eb
cpesar <cpesar1@gmail.com>
2025-01-19 22:48:47 -0700

    built sign up form and error handling for zod and prisma errors

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,52 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Metadata } from "next";
+import Link from "next/link";
+import SignUpForm from "./sign-up-form";
+import { ShoppingBagIcon } from "@/public/images/shopping-bags";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+const SignUpPage = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string;
+  }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
+
+  const session = await auth();
+
+  if (session) {
+    redirect(callbackUrl || "/");
+  }
+
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <ShoppingBagIcon width={100} height={100} />
+          </Link>
+          <CardTitle className="text-center">Create Account</CardTitle>
+          <CardDescription className="text-center">
+            Enter your information below to sign up
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <SignUpForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignUpPage;
