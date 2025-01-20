08451e04c492205a5585c6aad8b28d6b929c14e6
cpesar <cpesar1@gmail.com>
2025-01-19 23:08:29 -0700

    added the user role to the session with jwt callback

--- auth.ts
+++ auth.ts
@@ -57,12 +57,32 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       //set the user ID from the token
       session.user.id = token.sub;
+      //set the user role from the token
+      session.user.role = token.role;
+      // set the user name from the token
+      session.user.name = token.name;
 
       //if there is an update, set the user name
       if (trigger === "update") {
         session.user.name = user.name;
       }
       return session;
+    },
+    async jwt({ token, user, session, trigger }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+        // if user has no name, then use email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+          // update the db to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
